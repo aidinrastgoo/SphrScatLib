336e29f246b87141d3d27b0485aecaf0a0d4defa
Aidin Rastgoo <aidinrastgoo@gmail.com>
2023-12-25 11:03:55 +0100

    set the coefficient for upper hemishpere

--- src/SphrScatLib.jl
+++ src/SphrScatLib.jl
@@ -6,6 +6,7 @@ include("compute_U.jl")
 include("compute_coefficient_lower_hemisphere.jl")
 include("compute_coefficient_out_of_hemisphere.jl")
 include("Sphere.jl")
+include("compute_coefficient_upper_hemishpere.jl")
 
 
 export calculate_η
@@ -13,5 +14,6 @@ export generate_U_matrix
 export calculate_LH_A2
 export calculate_OH_E
 export calculate_in
+export calculate_HH_A1
 
 end # module SphrScatLib
