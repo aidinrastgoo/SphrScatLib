f7738b161613a0760fe2aa830bd27fc6c6f36e5f
Aidin Rastgoo <aidinrastgoo@gmail.com>
2023-12-25 10:20:49 +0100

    update Test

--- src/SphrScatLib.jl
+++ src/SphrScatLib.jl
@@ -4,10 +4,14 @@ import Pkg
 include("compute_η.jl")
 include("compute_U.jl")
 include("compute_coefficient_lower_hemisphere.jl")
+include("compute_coefficient_out_of_hemisphere.jl")
+include("Sphere.jl")
 
 
 export calculate_η
 export generate_U_matrix
 export calculate_LH_A2
+export calculate_OH_E
+export calculate_in
 
 end # module SphrScatLib
--- test/test.jl
+++ test/test.jl
@@ -1,15 +1,13 @@
 using LinearAlgebra
-ε_r2 = .2
-ε_r1 = .8
-l_max = 10
+ε_r2 = 1
+ε_r1 = 1
+l_max = 100
 radius_a = 0.0
 radius_b = radius_a + 0.9
 Amplitude = 1.0
-
-U = generate_U_matrix(l_max)
-η = calculate_η(l_max, ε_r1, ε_r2)
-@test 1.0 == 1.0
 A2 = calculate_LH_A2(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)
+A_S = calculate_in(Amplitude, radius, ε_r1, n_max, k_max)
+@test isapprox(A2,A_S)
 
 
 
