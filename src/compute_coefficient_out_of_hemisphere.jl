1d3b1d48ab7f2f1099e2dfdf99ddab0e4cb63432
Aidin Rastgoo <aidinrastgoo@gmail.com>
2024-01-22 19:29:57 +0100

    Repair the Error

--- src/compute_coefficient_out_of_hemisphere.jl
+++ src/compute_coefficient_out_of_hemisphere.jl
@@ -2,77 +2,62 @@ using LinearAlgebra
 include("compute_U.jl")
 include("compute_η.jl")
 include("compute_E_PEC.jl")
-#include("compute_coefficient_lower_hemisphere.jl")
 
-#function calculate_first_Part(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)  
-#    η = calculate_η(l_max, ε_r1, ε_r2)
-#    U = generate_U_matrix(l_max)
-#    A2 = calculate_LH_A2(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)
-#    inside = zeros(Float64, l_max)
-#    effect = zeros(Float64, l_max)
-#    for l in 0:l_max-1  
-#        for m in 0:l_max-1
-#            # check the Sign if the result is not relaiable
-#            effect[l+1] =  Amplitude * U[l+1, 2] * ( radius_b * (  ( η[l+1] * ε_r1 * l) - η[l+1] + (ε_r2 * l * ((-1)^(l+1)))  - ((-1)^(l+1))  ))   
-#            inside[l+1] = U[l+1, m+1] * A2[l+1] * l * (radius_a^((2*l)+1)) * (radius_b^(-l-1)) * ( (η[l+1] * (1-ε_r1)) + (((-1)^(l+m)) * (1- ε_r2) ) )
-#        end
-#    end
-#    return effect - inside
-#end
-
-#function calculate_OH_E(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)  #out of Hiemspere
-#    η = calculate_η(l_max, ε_r1, ε_r2)
-#    U = generate_U_matrix(l_max)
-#    part1 = calculate_first_Part(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max) 
-#    part2 = zeros(Float64, l_max, l_max)
-    
-#    for l in 0:l_max-1  
-#        for m in 0:l_max-1
-            
-#            part2[l+1, m+1] = U[l+1, m+1] * (radius_b^(-l-1)) * ( (η[l+1] * ε_r1 * l)  + ( η[l+1] * (l+1) ) + ( ε_r2 * ((-1)^(l+m)) * l) + ( (l+1) * ((-1)^(l+m)) ) )
-       
-#        end
-#    end
-#    return inv(part2) * part1
-#end
 function Part(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)
-    η = calculate_η( l_max, ε_r1, ε_r2)
-    U = generate_U_matrix(l_max)
-    part1 = zeros(Float64, l_max, l_max)
-    part2 = zeros(Float64, l_max)
-    E_PEC = compute_E_PEC(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)
-    for l in 0:l_max-1  
-        for m in 0:l_max-1   
-            part1[l+1 , m+1 ] =  radius_b^(-l-1) * (1 - η[l+1]) * (1-((l+1) * ε_r1 * ε_r2)) * U[l+1, m+1]
-            part2[l+1] = Amplitude * radius_b * ((1-(η[l+1] * (-1)^(1-l)))+(ε_r2 - (η[l+1] * ε_r1 * (-1)^(1-l)))) * U[l+1, 2]
-        end
-    end 
-    Part3 = E_PEC' * part1
-
-    return - Part3' - part2
-
-end
-
-function Part_b(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)
     η = calculate_η(l_max, ε_r1, ε_r2)
     U = generate_U_matrix(l_max)
-    term = zeros(Float64, l_max, l_max)
+    part = zeros(Float64, l_max, l_max)
+    
     for l in 0:l_max-1  
-        for m in 0:l_max-1
-            term[l+1, m+1] =  radius_b^(-l-1) * U[l+1, m+1] * ((1-η[l+1]) - ((l+1) * (ε_r2 - (η[l+1] * ε_r1))))
+        for m in 0:l_max-1   
+            part[l+1, m+1] = -(((η[l+1])^2 * ε_r1 * (2*l + 1)) + (ε_r2 * ((-1)^(l+m)) * (2*l + 1))) * (radius_b^(-l-2)) * U[l+1, m+1] 
         end
-    end
-    return inv(term)
+    end 
 
+    return part
 end
 
+function Part_PEC(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)
+    E_PEC = compute_E_PEC(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)
+    part = Part(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)
 
-function calculate_OH_E(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)  #out of Hiemspere
-    if ε_r1 != ε_r2
-    term1 = Part_b(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)
-    else
-        term1 = 0
-    end
- term2 = Part(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)
-    return  term1 * term2
+    return part * E_PEC 
+end
+
+function Part_Amplitude(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)
+    η = calculate_η(l_max, ε_r1, ε_r2)
+    U = generate_U_matrix(l_max)
+    part = zeros(Float64, l_max)
+    
+    for l in 0:l_max-1  
+        part[l+1] = Amplitude * ((η[l+1] * l * ε_r1) - η[l+1] + (ε_r2 * l * ((-1)^(l+1))) - ((-1)^(l+1))) * U[2, l+1] 
+    end 
+
+    return part
+end
+
+function Part_Out(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)
+    η = calculate_η(l_max, ε_r1, ε_r2)
+    U = generate_U_matrix(l_max)
+    part = zeros(Float64, l_max, l_max)
+    
+    for m in 0:l_max-1  
+        for l in 0:l_max-1   
+            t1 = η[l+1] * ε_r1 * l 
+            t2 = η[l+1] * (m+1)
+            t3 = ε_r2 * l * ((-1)^(l+m))
+            t4 = (m+1) * (-1)^(l+m)
+            part[l+1, m+1] = (t1 + t2 + t3 + t4) * radius_b^(-m-2) * U[l+1, m+1]
+        end
+    end 
+
+    return part
+end
+
+function calculate_OH_E(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)  #out of Hemisphere
+    PEC = Part_PEC(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)
+    Ampli = Part_Amplitude(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)
+    Out = Part_Out(Amplitude, radius_a, radius_b, ε_r1, ε_r2, l_max)
+
+    return inv(Out) * (Ampli + PEC)
 end
